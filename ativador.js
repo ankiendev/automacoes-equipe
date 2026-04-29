1e925c67352a55fd72af33a9e7791ecb41f3123b
ankiendev <carloslanguage@outlook.com>
2026-04-29 03:56:46 -0300

    Create ativador.js

---
+++ ativador.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Icarus Pro - Ativador
+// @version      1.0
+// @match        https://web.pontoicarus.com.br/*
+// @require      https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/ponto.user.js
+// @grant        none
+// ==/UserScript==
+
+// Este script é apenas um atalho. 
+// O código real está hospedado no GitHub do ankiendev.
