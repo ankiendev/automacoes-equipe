f885cb0e1e8bea92381f89f60a0288332e3dbfc9
ankiendev <carloslanguage@outlook.com>
2026-04-29 04:13:13 -0300

    Update ativador.user.js

--- ativador.user.js
+++ ativador.user.js
@@ -7,7 +7,7 @@
 // @match        https://web.pontoicarus.com.br/*
 // @match        https://monitoring.cloud.kiper.com.br/condominiums/*
 // @require      https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/ponto.user.js?v=1
-// @require      https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/payload.user.js?v=1
+// @require      https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/ativador.user.js?v=1
 // @downloadURL  https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/central-equipe.user.js
 // @updateURL    https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/central-equipe.user.js
 // @grant        none
