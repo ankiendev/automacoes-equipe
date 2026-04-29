8ce022c13ccecefb68729661096b82350328adc2
ankiendev <carloslanguage@outlook.com>
2026-04-29 10:05:05 -0300

    Update ativador.user.js

--- ativador.user.js
+++ ativador.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Central de Automações - Equipe
 // @namespace    http://tampermonkey.net/
-// @version      3.0
+// @version      3.1
 // @description  Instalador único para todas as ferramentas da equipe.
 // @author       ankiendev
 // @match        https://web.pontoicarus.com.br/*
 // @match        https://monitoring.cloud.kiper.com.br/condominiums/*
 // @require      https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/ponto.user.js?v=3000
-// @require      https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/licencas.user.js?v=3
+// @require      https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/licencas.user.js?v=4
 // @downloadURL  https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/ativador.user.js
 // @updateURL    https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/ativador.user.js
 // @grant        none
