db3d708cb5f740d90169e9a8a82b89dd9b2cd343
ankiendev <carloslanguage@outlook.com>
2026-04-29 03:57:54 -0300

    Update ativador.js

--- ativador.js
+++ ativador.js
@@ -1,10 +1,16 @@
 // ==UserScript==
 // @name         Icarus Pro - Ativador
+// @namespace    http://tampermonkey.net/
 // @version      1.0
+// @description  Ativador das automações da equipe.
+// @author       ankiendev
 // @match        https://web.pontoicarus.com.br/*
 // @require      https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/ponto.user.js
+// @downloadURL  https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/ativador-icarus.user.js
+// @updateURL    https://raw.githubusercontent.com/ankiendev/automacoes-equipe/main/ativador-icarus.user.js
 // @grant        none
 // ==/UserScript==
 
 // Este script é apenas um atalho. 
-// O código real está hospedado no GitHub do ankiendev.
+// O código real está hospedado no arquivo 'ponto.user.js'.
+console.log("Icarus Pro: Ativador carregado com sucesso.");
